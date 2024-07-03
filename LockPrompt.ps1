2f99a282aa82a647f2f6d369e6786d551b2d8167
TheBrightSoul <dabrightsoul13@gmail.com>
2024-07-03 13:46:20 +0530

    Added a variety of messages to display some cool messages to remind for break

--- LockPrompt.ps1
+++ LockPrompt.ps1
@@ -1,11 +1,14 @@
-param (
-    [string]$message = "Soul! You need a Break! Turn off the System!"
-)
+# Read messages from file
+$messagesFile = Join-Path $PSScriptRoot "messages.txt"
+$messages = Get-Content $messagesFile
+
+# Select a random message
+$randomMessage = $messages | Get-Random
 
 Add-Type -AssemblyName PresentationFramework
 
 $buttonType = [System.Windows.MessageBoxButton]::YesNo
-$messageBoxResult = [System.Windows.MessageBox]::Show($message, "Break Reminder", $buttonType)
+$messageBoxResult = [System.Windows.MessageBox]::Show($randomMessage, "Break Reminder", $buttonType)
 
 if ($messageBoxResult -eq [System.Windows.MessageBoxResult]::Yes) {
     exit 1  # Yes
