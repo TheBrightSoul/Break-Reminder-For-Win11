1c0e275e43b01a0f46b6fa1a70f55e9b50cd0776
TheBrightSoul <dabrightsoul13@gmail.com>
2024-07-03 13:58:24 +0530

    Upgraded the GUI using Windows Presentation Foundation (WPF)

--- LockPrompt.ps1
+++ LockPrompt.ps1
@@ -1,3 +1,5 @@
+Add-Type -AssemblyName PresentationFramework, PresentationCore, WindowsBase
+
 # Read messages from file
 $messagesFile = Join-Path $PSScriptRoot "messages.txt"
 $messages = Get-Content $messagesFile
@@ -5,13 +7,55 @@ $messages = Get-Content $messagesFile
 # Select a random message
 $randomMessage = $messages | Get-Random
 
-Add-Type -AssemblyName PresentationFramework
+# XAML for the window
+[xml]$xaml = @"
+<Window xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        Title="Break Reminder" Height="200" Width="400" WindowStartupLocation="CenterScreen">
+    <Grid Margin="10">
+        <Grid.RowDefinitions>
+            <RowDefinition Height="*"/>
+            <RowDefinition Height="Auto"/>
+        </Grid.RowDefinitions>
 
-$buttonType = [System.Windows.MessageBoxButton]::YesNo
-$messageBoxResult = [System.Windows.MessageBox]::Show($randomMessage, "Break Reminder", $buttonType)
+        <TextBlock x:Name="MessageText" Grid.Row="0" TextWrapping="Wrap" FontSize="16" VerticalAlignment="Center" HorizontalAlignment="Center"/>
 
-if ($messageBoxResult -eq [System.Windows.MessageBoxResult]::Yes) {
-    exit 1  # Yes
+        <StackPanel Grid.Row="1" Orientation="Horizontal" HorizontalAlignment="Center">
+            <Button x:Name="TakeBreakButton" Content="Take a Break" Width="100" Margin="5"/>
+            <Button x:Name="SkipButton" Content="Skip" Width="100" Margin="5"/>
+        </StackPanel>
+    </Grid>
+</Window>
+"@
+
+$reader = New-Object System.Xml.XmlNodeReader $xaml
+$window = [Windows.Markup.XamlReader]::Load($reader)
+
+# Get controls
+$messageText = $window.FindName("MessageText")
+$takeBreakButton = $window.FindName("TakeBreakButton")
+$skipButton = $window.FindName("SkipButton")
+
+# Set the message
+$messageText.Text = $randomMessage
+
+# Button click events
+$takeBreakButton.Add_Click({
+    $window.DialogResult = $true
+    $window.Close()
+})
+
+$skipButton.Add_Click({
+    $window.DialogResult = $false
+    $window.Close()
+})
+
+# Show the window
+$result = $window.ShowDialog()
+
+# Return the result
+if ($result) {
+    exit 1  # Take a break
 } else {
-    exit 0  # No
+    exit 0  # Skip
 }
